edf1e759d06f05d5b7767ef01c3a2f87eb717a9e
nukka123 <nukamiso.master@gmail.com>
2025-07-29 23:30:33 +0900

    fix.

--- Tools/SFSymbolKeyCodeGen/Tests/SFSymbolKeyResourceTests/SFSymbolKeyResourceTests.swift
+++ Tools/SFSymbolKeyCodeGen/Tests/SFSymbolKeyResourceTests/SFSymbolKeyResourceTests.swift
@@ -11,7 +11,7 @@ struct SFSymbolKeyResourceTests {
             $0.config.version < $1.config.version
         }
         #expect(specs.count == 23)
-        #expect(specs.first!.config.version == "1.0")
-        #expect(specs.last!.config.version == "7.0")
+        #expect(specs.first!.config.version.string == "1.0")
+        #expect(specs.last!.config.version.string == "7.0")
     }
 }
